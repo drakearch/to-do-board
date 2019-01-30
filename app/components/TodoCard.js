0bce193d1926aa20a2961bdad5806fcb4ca73634
drakessn <drakessn@gmail.com>
2019-01-29 21:18:44 -0500

    Todo Cards!

--- app/components/TodoCard.js
+++ app/components/TodoCard.js
@@ -20,18 +20,35 @@ class TodoCard extends Component {
 	}
 
   render() {
+		
+		let priority;
+		if(this.props.todo.priority === 'high') {
+			priority = <div className="chip red white-text">{ this.props.todo.priority }</div>
+		}
+		else {
+			if( this.props.todo.priority === 'medium') {
+				priority = <div className="chip green white-text">{ this.props.todo.priority }</div>
+			}
+			else {
+				priority = <div className="chip yellow darken-3 white-text">{ this.props.todo.priority }</div>
+			}
+		}
+
     return (
       <div className="col s12 m12 l6">
-        <div className="card blue-grey darken-1">
-					<div className="card-content white-text">
+        <div className="card">
+					<div className="card-content indigo darken-1 white-text center-align">
 						<span className="card-title">{ this.props.todo.title }</span>
+						{ priority }
+					</div>
+					<div className="card-content black-text left-align">
 						<p>{ this.props.todo.description }</p>
 					</div>
-					<div className="card-action">
-						<button onClick={ this.handleEdit } className="btn light-blue darken-4" style={{margin: '4px'}}>
+					<div className="card-action right-align">
+						<button onClick={ this.handleEdit } className="btn-floating indigo" style={{margin: '0 5px'}}>
 							<i className="material-icons">edit</i>
 						</button>
-						<button onClick={ this.handleDelete } className="btn halfway-fab waves-effect waves-light red" style={{margin: '4px'}}>
+						<button onClick={ this.handleDelete } className="btn-floating red" style={{margin: '0 5px'}}>
 							<i className="material-icons">delete</i>
 						</button>
 					</div>
